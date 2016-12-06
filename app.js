52784ba942f204f749d100fb3a926c37c377eae9
Phillip Lorenzo <philliplorenzo@Phillips-MacBook-Pro.local>
2016-12-06 13:54:36 -0800

    Initial setup. Need to look into why load time is heavy.

---
+++ app.js
@@ -0,0 +1,64 @@
+const express = require('express')
+const path = require('path')
+const logger = require('morgan')
+const favicon = require('serve-favicon')
+const cookieParser = require('cookie-parser')
+const bodyParser = require('body-parser')
+
+const webpack = require('webpack')
+const webpackMiddleware = require('webpack-dev-middleware')
+const webpackHotMiddleware = require('webpack-hot-middleware')
+const config = require('./webpack.config.js')
+
+const app = express()
+
+app.use(favicon(path.join(__dirname, 'client', 'favicon.ico')))
+app.use(logger('dev'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cookieParser())
+
+const compiler = webpack(config)
+const middleware = webpackMiddleware(compiler, {
+  publicPath: config.output.publicPath,
+  contentBase: 'src',
+  stats: {
+    colors: true,
+    hash: false,
+    timings: true,
+    chunks: false,
+    chunkModules: false,
+    modules: false,
+  }
+})
+
+app.use(middleware)
+app.use(webpackHotMiddleware(compiler))
+
+app.get('*', (req, res) => {
+  res.write(middleware.fileSystem.readFileSync(path.join(__dirname, 'dist/index.html')))
+})
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use((err, req, res) => {
+    res.status(err.status || 500)
+    res.render('error', {
+      message: err.message,
+      error: err
+    })
+  })
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use((err, req, res) => {
+  res.status(err.status || 500)
+  res.render('error', {
+    message: err.message,
+    error: {}
+  })
+})
+
+module.exports = app
